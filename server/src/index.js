a5d7e854da88b8e9e8515a222747e0e1a92e9735
tykowale <tykowale@gmail.com>
2020-09-09 17:46:11 -0700

    add all crud operations

--- server/src/index.js
+++ server/src/index.js
@@ -1,38 +1,61 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String
-}
-`;
+const _ = require("lodash");
 
 let links = [{
   id: "link-0",
   url: "www.howtographql.com",
   description: "Fullstack tutorial for GraphQL",
 }];
+let idCount = links.length;
+
+function findLink(id) {
+  return _.find(links, (i) => {
+    return i.id === id;
+  });
+}
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    link: (parent, args) => {
+      return findLink(args.id);
+    },
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+
+      return link;
+    },
+    updateLink: (parent, args) => {
+      const link = findLink(args.id);
+      if (args.url) {
+        link.url = args.url;
+      }
+      if (args.description) {
+        link.description = args.description;
+      }
+
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      return _.remove(links, (i) => {
+        return i.id === args.id;
+      })[0];
+    },
+  },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers,
 });
 
